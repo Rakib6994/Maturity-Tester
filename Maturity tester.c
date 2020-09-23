3711e08517f994b60c499ff0e5d5585358c9bd63
Rakib <rkbhasan28@gmail.com>
2020-09-23 15:40:49 +0600

    Maturity Tester

---
+++ Maturity tester.c
@@ -0,0 +1,293 @@
+#include<stdio.h>
+int main()
+{
+    int x=0,n;
+    printf("                                                      <<<RHR>>>");
+
+    printf("                                     welcome to Maturity tester .\n");
+    printf("         Answer all the questions honestly to get the accurate result.\n\n\n");
+    printf("How old are you?\n\n");
+    printf("1.7 or younger.\n");
+    printf("2.8-9 years old.\n");
+    printf("3.10-11 years old.\n");
+    printf("4.12-15 years old.\n");
+    printf("5.16+..\n");
+    scanf("%d",&n);
+    printf("\n\n");
+    switch(n)
+    {
+    case 1:
+        x=x+10;
+        break;
+    case 2:
+        x=x+20;
+        break;
+    case 3:
+        x=x+30;
+        break;
+    case 4:
+        x=x+40;
+        break;
+    case 5:
+        x=x+50;
+        break;
+    }
+
+    printf("Who do you want to be like?\n\n");
+    printf("1.someone special.\n");
+    printf("2.superman.\n");
+    printf("3.Obama.\n");
+    printf("4.My parents.\n");
+    printf("5.Taylor Swift.\n");
+     scanf("%d",&n);
+      printf("\n\n");
+     switch(n)
+       {
+    case 1:
+        x=x+50;
+        break;
+    case 2:
+        x=x+20;
+        break;
+    case 3:
+        x=x+40;
+        break;
+    case 4:
+        x=x+10;
+        break;
+    case 5:
+        x=x+30;
+        break;
+    }
+    printf("Do you have a crush on a actor/actress or singer?\n\n");
+    printf("1.I don't know.\n");
+    printf("2.I'm not sure,but i think most of them are hot.\n");
+    printf("3.Yeah,maybe one or two.\n");
+    printf("4.No,they got their lives,i got mine.\n");
+    printf("5.Yes,and one day we'll be together.\n");
+     scanf("%d",&n);
+      printf("\n\n");
+     switch(n)
+       {
+    case 1:
+        x=x+30;
+        break;
+    case 2:
+        x=x+40;
+        break;
+    case 3:
+        x=x+20;
+        break;
+    case 4:
+        x=x+50;
+        break;
+    case 5:
+        x=x+10;
+        break;
+    }
+    printf("Where do you like to go on the weekends?\n\n");
+    printf("1.ZOO.\n");
+    printf("2.Park.\n");
+    printf("3.Club.\n");
+    printf("4.Mall.\n");
+    printf("5.Disneyland.\n");
+     scanf("%d",&n);
+      printf("\n\n");
+     switch(n)
+       {
+    case 1:
+        x=x+20;
+        break;
+    case 2:
+        x=x+30;
+        break;
+    case 3:
+        x=x+50;
+        break;
+    case 4:
+        x=x+40;
+        break;
+    case 5:
+        x=x+10;
+        break;
+    }
+    printf("If your computer gets hacked,you will.....\n\n");
+    printf("1.Ask for help.\n");
+    printf("2.Try to fix it the best you can.\n");
+    printf("3.Freak out!\n");
+    printf("4.Smash it into pieces.\n");
+    printf("5.It's fine to get a new one.\n");
+     scanf("%d",&n);
+      printf("\n\n");
+     switch(n)
+       {
+    case 1:
+        x=x+40;
+        break;
+    case 2:
+        x=x+50;
+        break;
+    case 3:
+        x=x+20;
+        break;
+    case 4:
+        x=x+10;
+        break;
+    case 5:
+        x=x+30;
+        break;
+    }
+    printf("What will you do,if your teacher leave the room ?\n\n");
+    printf("1.Yell and scream.\n");
+    printf("2.Dance on the floor and act silly.\n");
+    printf("3.Talk with classmates.\n");
+    printf("4.Listen to music through earphones.\n");
+    printf("5.Sit quietly and catch up on work.\n");
+     scanf("%d",&n);
+      printf("\n\n");
+     switch(n)
+       {
+    case 1:
+        x=x+20;
+        break;
+    case 2:
+        x=x+10;
+        break;
+    case 3:
+        x=x+30;
+        break;
+    case 4:
+        x=x+40;
+        break;
+    case 5:
+        x=x+50;
+        break;
+    }
+    printf("If you get caught cheating on a test what will you do ?");
+    printf("1.Pretend you don't know anything.\n");
+    printf("2.Blame a friend.\n");
+    printf("3.Run away.\n");
+    printf("4.Feel guilty about it and accept the consequences.\n");
+    printf("5.Yell at the teacher and call him liar.\n");
+     scanf("%d",&n);
+      printf("\n\n");
+     switch(n)
+       {
+    case 1:
+        x=x+40;
+        break;
+    case 2:
+        x=x+30;
+        break;
+    case 3:
+        x=x+20;
+        break;
+    case 4:
+        x=x+50;
+        break;
+    case 5:
+        x=x+10;
+        break;
+    }
+    printf("Someone insulted your best friend behind his back,you will....\n\n");
+    printf("1.Beat him.\n");
+    printf("2.Tell your friend about it.\n");
+    printf("3.Insult him.\n");
+    printf("4.Ignore him,whatever...\n");
+    printf("5.Give him a dirty look.\n");
+     scanf("%d",&n);
+      printf("\n\n");
+     switch(n)
+       {
+    case 1:
+        x=x+10;
+        break;
+    case 2:
+        x=x+50;
+        break;
+    case 3:
+        x=x+20;
+        break;
+    case 4:
+        x=x+30;
+        break;
+    case 5:
+        x=x+40;
+        break;
+    }
+    printf("Your mom says you can't have the cookies.\n\n");
+    printf("1.That's ok,I hate you.\n");
+    printf("2.Fine...\n");
+    printf("3.No,I want them now.\n");
+    printf("4.I'm on a diet anyway.\n");
+    printf("5.You cant tell me what to do! I'm too mature.\n");
+     scanf("%d",&n);
+      printf("\n\n");
+     switch(n)
+       {
+    case 1:
+        x=x+30;
+        break;
+    case 2:
+        x=x+20;
+        break;
+    case 3:
+        x=x+10;
+        break;
+    case 4:
+        x=x+40;
+        break;
+    case 5:
+        x=x+50;
+        break;
+    }
+    printf("Where do you see yourself after 10 years?\n\n");
+    printf("1.Starting my own business.\n");
+    printf("2.Blissfully in love with my high school crush.\n");
+    printf("3.spending time in jail.\n");
+    printf("4.Probably divorced.\n");
+    printf("5.Being a successful bingo player.\n");
+     scanf("%d",&n);
+      printf("\n\n");
+     switch(n)
+       {
+    case 1:
+        x=x+50;
+        break;
+    case 2:
+        x=x+40;
+        break;
+    case 3:
+        x=x+10;
+        break;
+    case 4:
+        x=x+30;
+        break;
+    case 5:
+        x=x+20;
+        break;
+    }
+    printf("YOUR MATURITY LEVEL:\n");
+    if(x>=100 && x<=150)
+    {
+         printf("You are a child.You act like a kid.\n");
+    }
+     if(x>=151 && x<=250)
+    {
+         printf("You are still a kid at heart.\n");
+    }
+     if(x>=251 && x<=350)
+    {
+         printf("You are teen.Your heart is half kid,half-adult.\n");
+    }
+     if(x>=351 && x<=450)
+    {
+         printf("You are young adult! You have a playful nature,but you are more on the grown up side. \n");
+    }
+     if(x>=451 && x<=500)
+    {
+         printf("You are adult! if you are not,you matured really fast.\n");
+    }
+
+    return 0;
+}
